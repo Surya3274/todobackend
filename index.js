0c88c7f946f061f81783bf7b0971186114e0dc60
D SURYA <dhs32747138@gmail.com>
2024-07-29 17:52:34 +0530

    added validatoins

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo",function (req,res) {
-    
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);  
+    if (!parsePayload.success) {
+        res.status(411).json({
+            msg:"Wrong input"
+        });
+
+    }
+    else {
+        return;
+    }
 })
 
 app.get("/todos",function (req,res) {
@@ -13,7 +24,17 @@ app.get("/todos",function (req,res) {
 })
 
 app.put("/todo",function (req,res) {
-    
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);  
+    if (!parsePayload.success) {
+        res.status(411).json({
+            msg:"Wrong input"
+        });
+
+    }
+    else {
+        return;
+    }
 })
 
 app.delete("/todo",function (req,res) {
